59bace01b6258ea46882023c4c44580fd5e5053e
Emily Redler <esredler@gmail.com>
2014-11-12 19:09:21 -0500

    initial upload

---
+++ 02ArrayList/collapse.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class collapse {
+
+    public static void collapseDuplicates(ArrayList<Integer> L){
+	for (int i = 1; i < L.size(); i++){
+	    System.out.println("arraylist when i is " + i + ":\t" + L.toString());
+	    if (L.get(i-1).equals(L.get(i))){
+		System.out.println(L.get(i-1) + " equals " + L.get(i)); 
+		System.out.println("index " + i + " (which is " + L.get(i) + ") will be removed");
+		L.remove(i);
+		i--;
+	    }
+	}	
+    }
+
+    public static Integer makeInt(int i){
+	Integer ans = new Integer(i);
+	return ans;
+    }
+
+    public static void main(String[]args){
+
+	ArrayList<Integer> L = new ArrayList<Integer>();
+	L.add(makeInt(1));
+	L.add(makeInt(9));
+	L.add(makeInt(3));
+	L.add(makeInt(3));
+	L.add(makeInt(4));
+	L.add(makeInt(2));
+	L.add(makeInt(2));
+	L.add(makeInt(2));
+	L.add(makeInt(7));
+	L.add(makeInt(8));
+	L.add(makeInt(8));
+	L.add(makeInt(8));
+	L.add(makeInt(8));
+	L.add(makeInt(9));
+	System.out.println(L);
+	collapseDuplicates(L);
+	System.out.println(L);
+    }
+
+}
