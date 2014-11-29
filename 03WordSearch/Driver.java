383e63d5a686d5eaac44227b81d10bb7cc4094f6
Emily Redler <esredler@gmail.com>
2014-11-29 15:12:16 -0500

    removed methods from main and streamlined

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -2,8 +2,47 @@ import java.util.*;
 
 public class Driver {
 
+    public static void addWordInDriver(WordSearch example, int numrows, int numcols, Random r, ArrayList<String> wordsUsed, String[] wordBank, int effort, int howManyWords){
+	for (int i = 0; i < wordBank.length; i++){
+	    boolean success = false;
+	    int trynum = 0;
+	    int spot = r.nextInt(wordBank.length);
+	    if (wordsUsed.contains(wordBank[spot])){
+		trynum = effort;
+	    }
+	    if (wordsUsed.size() > howManyWords){
+		trynum = effort;
+		i = wordBank.length;
+	    }
+	    while (trynum < effort){
+		int dx = r.nextInt(3) - 1;
+		int dy = r.nextInt(3) - 1;
+		int rw = r.nextInt(numrows);
+		int cl = r.nextInt(numcols);
+		success = example.addWord(wordBank[spot], rw, cl, dx, dy);
+		if (success){
+		    wordsUsed.add(wordBank[spot]);
+		    trynum = effort;
+		}
+		trynum++;
+	    }
+	   
+	}
+    }
+
+    public static void printFinal(boolean ansKey, WordSearch example, ArrayList<String> wordsUsed){
+	if (ansKey == true){
+	    System.out.println(example.toString());
+	    System.out.println(wordsUsed);
+	} else {
+	    example.fillEmpty();
+	    System.out.println(example.toString());
+	    System.out.println(wordsUsed);
+	}
+    }
+
     public static void main(String[]args){
-	
+
 	try {
 	    int numrows = Integer.parseInt(args[0]);
 	    int numcols = Integer.parseInt(args[1]);
@@ -17,12 +56,12 @@ public class Driver {
 		}
 	    } catch (Exception e) {}
 
+	    Random r = new Random();
+
 	    try {
 		int seed = Integer.parseInt(args[2]);
-		Random r = new Random(seed);
-	    } catch (Exception e) {
-		Random r = new Random();
-	    }
+	        r = new Random(seed);
+	    } catch (Exception e) {}
 
 	    ArrayList<String> wordsUsed = new ArrayList<String>();
 
@@ -30,38 +69,9 @@ public class Driver {
 
 	    WordSearch example = new WordSearch(numrows,numcols);
 
-	    for (int i = 0; i < wordBank.length; i++){
-		boolean success = false;
-		int trynum = 0;
-		int spot = r.nextInt(wordBank.length);
-		if (wordsUsed.contains(wordBank[spot])){
-		    trynum = effort;
-		}
-		if (wordsUsed.size() > howManyWords){
-		    trynum = effort;
-		    i = wordBank.length;
-		}
-		while (trynum < effort){
-		    int dx = r.nextInt(3) - 1;
-		    int dy = r.nextInt(3) - 1;
-		    int rw = r.nextInt(numrows);
-		    int cl = r.nextInt(numcols);
-		    if (example.addWord(wordBank[spot], rw, cl, dx, dy)){
-			wordsUsed.add(wordBank[spot]);
-		    }
-		    trynum++;
-		}
-	   
-	    }
+	    addWordInDriver(example, numrows, numcols, r, wordsUsed, wordBank, effort, howManyWords);
         
-	    if (ansKey == true){
-		System.out.println(example.toString());
-		System.out.println(wordsUsed);
-	    } else {
-		example.fillEmpty();
-		System.out.println(example.toString());
-		System.out.println(wordsUsed);
-	    }
+	    printFinal(ansKey, example, wordsUsed);
 
 	} catch (Exception e){
 	    System.out.println("\nPlease call Driver in the following format:\n\n\tDriver rows cols [randomSeed [answers] ]\n\nrows and cols should be integers.\nrandomSeed should be an integer.\nanswers should be used only if randomSeed is used and can only be the value 1 (for true).\n");
