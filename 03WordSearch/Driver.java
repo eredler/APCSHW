b2235334ae90d9f9e7e3a9a8160f9b9ab2267df7
Emily Redler <esredler@gmail.com>
2014-11-29 17:22:15 -0500

    replaced wordListCopy, fixed printing of wordsUsed, removed howMany

--- 03WordSearch/Driver.java
+++ 03WordSearch/Driver.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 public class Driver {
 
-    public static void addWordInDriver(WordSearch example, int numrows, int numcols, Random r, ArrayList<String> wordsUsed, String[] wordBank, int effort, int howManyWords){
+    public static void addWordInDriver(WordSearch example, int numrows, int numcols, Random r, ArrayList<String> wordsUsed, String[] wordBank, int effort){
 	for (int i = 0; i < wordBank.length; i++){
 	    boolean success = false;
 	    int trynum = 0;
 	    int spot = r.nextInt(wordBank.length);
-	    if (wordsUsed.contains(wordBank[spot])){
-		trynum = effort;
-	    }
-	    if (wordsUsed.size() > howManyWords){
-		trynum = effort;
+	    if (wordBank[spot].length() <= 3){
 		i = wordBank.length;
-	    }
+	    } else  if (wordsUsed.contains(wordBank[spot])){
+		i = wordBank.length;	i = wordBank.length;
+	    } else {
 	    while (trynum < effort){
 		int dx = r.nextInt(3) - 1;
 		int dy = r.nextInt(3) - 1;
@@ -26,18 +24,17 @@ public class Driver {
 		}
 		trynum++;
 	    }
-	   
+	    
+	    }
 	}
     }
 
-    public static void printFinal(boolean ansKey, WordSearch example, ArrayList<String> wordsUsed){
+    public static void printFinal(boolean ansKey, WordSearch example){
 	if (ansKey == true){
 	    System.out.println(example.toString());
-	    System.out.println(wordsUsed);
 	} else {
 	    example.fillEmpty();
 	    System.out.println(example.toString());
-	    System.out.println(wordsUsed);
 	}
     }
 
@@ -46,13 +43,33 @@ public class Driver {
 	return wordBank;
     }
 
+    public static void convertWordsUsed(ArrayList<String> wordsUsed){
+	for (int rowOn = 0; rowOn < wordsUsed.size()/4; rowOn++){
+	    System.out.format("%15s %15s %15s %15s %n", wordsUsed.get(4*rowOn), wordsUsed.get(rowOn+1+3*rowOn), wordsUsed.get(rowOn*2+2+2*rowOn), wordsUsed.get(rowOn*3+3+rowOn));
+	}
+	int remaining = wordsUsed.size() - (wordsUsed.size()/4)*4;
+	switch (remaining){
+	case 0:
+	    break;
+	case 1: 
+	    System.out.format("%15s %n", wordsUsed.get(wordsUsed.size()-1));
+	    break;
+	case 2: 
+	    System.out.format("%15s %15s %n", wordsUsed.get(wordsUsed.size()-2), wordsUsed.get(wordsUsed.size()-1));
+	    break;
+	case 3: 
+	    System.out.format("%15s %15s %15s %n", wordsUsed.get(wordsUsed.size()-3), wordsUsed.get(wordsUsed.size()-2), wordsUsed.get(wordsUsed.size()-1));
+	    break;
+	}
+    }
+
+
     public static void main(String[]args){
 
 	try {
 	    int numrows = Integer.parseInt(args[0]);
 	    int numcols = Integer.parseInt(args[1]);
 	    int effort = 10;
-	    int howManyWords = 20;
 	    boolean ansKey = false;	    
 	    ArrayList<String> wordsUsed = new ArrayList<String>();
 	    String[] wordBank = new String[0];
@@ -73,10 +90,10 @@ public class Driver {
 	    } catch (Exception e) {}
 
 
-	    addWordInDriver(example, numrows, numcols, r, wordsUsed, wordBank, effort, howManyWords);
+	    addWordInDriver(example, numrows, numcols, r, wordsUsed, wordBank, effort);
         
-	    printFinal(ansKey, example, wordsUsed);
-
+	    printFinal(ansKey, example);
+	    convertWordsUsed(wordsUsed);
 	} catch (Exception e){
 	    System.out.println("\nPlease call Driver in the following format:\n\n\tDriver rows cols [randomSeed [answers] ]\n\nrows and cols should be integers.\nrandomSeed should be an integer.\nanswers should be used only if randomSeed is used and can only be the value 1 (for true).\n");
 	}
