74ec1be9726c3becfd48605a6b9171f23d5491be
Emily Redler <esredler@gmail.com>
2014-12-04 13:17:14 -0500

    verified sort

--- 04OrderedSuperArray/tester.java
+++ 04OrderedSuperArray/tester.java
@@ -1,25 +1,6 @@
-public class tester{
+import java.util.*;
 
-    public static void verifySort(){
-	OrderedSuperArray mySort = new OrderedSuperArray(10);
-	OrderedSuperArray builtInSort = new OrderedSuperArray(10);
-	mySort.add("emily");
-	mySort.add("apple");
-	mySort.add("rainbow");
-	mySort.add("apricot");
-	mySort.add("computer");
-	mySort.add("zebra");
-	mySort.add("a");
-	builtInSort.add("emily");
-	builtInSort.add("apple");
-	builtInSort.add("rainbow");
-	builtInSort.add("apricot");
-	builtInSort.add("computer");
-	builtInSort.add("zebra");
-	builtInSort.add("a");
-	System.out.println("My sorting method: " + mySort.toString());
-	System.out.println("Built-in sorting method: " + builtInSort.toString());
-    }
+public class tester{
 
     public static void main(String[]args){
 	
@@ -29,46 +10,57 @@ public class tester{
 	//	OrderedSuperArray a = new OrderedSuperArray();
 	OrderedSuperArray b = new OrderedSuperArray(5);
 	/*	System.out.println(a.toString());
-	System.out.println(b.toString());
-	a.add("Hello");
-	System.out.println(a.toString());
-	b.resize(2);
-	System.out.println(b.toString());
-	System.out.println(a.size());
-	a.clear();
-	System.out.println(a.toString());
-	b.set(0, "yoyo");
-	System.out.println(b.toString());
-	System.out.println(b.get(0));
-	System.out.println(b.get(1));
-	b.add("hi");
-	System.out.println(b.toString());
-	b.add("yellow");
-	System.out.println(b.toString());
-	b.add("red",1);
-	System.out.println(b.toString());
-	b.remove(4);
-	System.out.println(b.toString());
-	System.out.println(b.remove(1));
+		System.out.println(b.toString());
+		a.add("Hello");
+		System.out.println(a.toString());
+		b.resize(2);
+		System.out.println(b.toString());
+		System.out.println(a.size());
+		a.clear();
+		System.out.println(a.toString());
+		b.set(0, "yoyo");
+		System.out.println(b.toString());
+		System.out.println(b.get(0));
+		System.out.println(b.get(1));
+		b.add("hi");
+		System.out.println(b.toString());
+		b.add("yellow");
+		System.out.println(b.toString());
+		b.add("red",1);
+		System.out.println(b.toString());
+		b.remove(4);
+		System.out.println(b.toString());
+		System.out.println(b.remove(1));
 
-	System.out.println(b.toString());*/
+		System.out.println(b.toString());*/
 	/*	b.add("yaaaaa");
-	b.add("zaaaaa");
-	b.add("xaaaaa");*/
+		b.add("zaaaaa");
+		b.add("xaaaaa");*/
 	/*	b.add("emily");
-	b.add("apple");
-	b.add("rainbow");
-	b.add("apricot");
-	b.add("computer");
-	b.add("zebra");
-	b.add("a");
-	System.out.println(b.toString());*/
+		b.add("apple");
+		b.add("rainbow");
+		b.add("apricot");
+		b.add("computer");
+		b.add("zebra");
+		b.add("a");
+		System.out.println(b.toString());*/
 	//	System.out.println(b.get(2));
 	//	System.out.println(b.get(70));
 	//	System.out.println(b.get(b.size()));
 	//	System.out.println(b.get(b.size() - 1));
 	//	b.sort();
-	verifySort();
+	SuperArray x = new SuperArray(10);
+	x.add("emily");
+	x.add("zebra");
+	x.add("apricot");
+	x.add("string");
+	x.add("computer");
+	x.add("apple");
+	x.add("a");
+	x.add("water");
+	System.out.println("before: " + x.toString());
+	x.sort();
+	System.out.println("after: " + x.toString());
     }
 
 }
