d8b5f22ee0f1360b27be8cd31d80d79aad822fac
eredler <esredler@gmail.com>
2014-12-11 12:10:06 -0500

    fixed ctof

--- tempConvert.java
+++ tempConvert.java
@@ -11,7 +11,8 @@ public class tempConvert extends JFrame implements ActionListener {
   private JButton convert, clear;
   private JLabel answer;
 
-  public Gui() {
+    public tempConvert() {
+
     this.setTitle("Temperature Converter");
     this.setSize(500, 500);
     this.setLocation(100, 100);
@@ -19,46 +20,46 @@ public class tempConvert extends JFrame implements ActionListener {
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     pane = this.getContentPane();
-    pane.setLayout(new FlowLayout());
+    pane.setLayout(new GridLayout(4,4));
     
     FtoC = new JButton("Fahrenheit to Celcius");
     CtoF = new JButton("Celcius to Fahrenheit");
     input = new JTextField(10);
-    answer = new JLabel("",null,JLabel.center);
+    answer = new JLabel("",null,JLabel.CENTER);
     
     FtoC.setActionCommand("FtoC");
-	  FtoC.addActionListener(this);
-  	CtoF.setActionCommand("CtoF");
-  	CtoF.addActionListener(this);
+    FtoC.addActionListener(this);
+    CtoF.setActionCommand("CtoF");
+    CtoF.addActionListener(this);
     
     pane.add(input);
-  	pane.add(FtoC);
-  	pane.add(CtoF);
-  	pane.add(answer);
-  }
-
+    pane.add(FtoC);
+    pane.add(CtoF);
+    pane.add(answer);
+  
+    }
   public void actionPerformed(ActionEvent e) {
     String action = e.getActionCommand();
   	if (action.equals("FtoC")){
-	  	String s = panel.getText();
+	  	String s = input.getText();
 	  	int num = Integer.parseInt(s);
-  		double ans = num;
+  		int ans = num;
   		ans = (ans-32)*5/9;
-	  	result.setText(""+ans);
+	  	answer.setText(""+ans);
   	}
 	    
   	if (action.equals("CtoF")){
 	    String s = input.getText();
 	    int num = Integer.parseInt(s);
-	    double ans = num;
+	    int ans = num;
 	    ans = ans * 9/5 + 32;
-	    result.setText(""+ans);		  
+	    answer.setText(""+ans);		  
   	} 
   }
   
   
  public static void main(String[] args) {
-        Gui x = new Gui();
+        tempConvert x = new tempConvert();
         x.setVisible(true);
    }
   }   
