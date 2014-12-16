3a993da67c4b84b8ebff526ed4db97fbe467a34b
eredler <esredler@gmail.com>
2014-12-16 12:27:26 -0500

    modified BubbleSort

---
+++ sortTester.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class sortTester {
+
+    public static void main(String[]args){
+	int[] a = new int[20];
+	Random r = new Random();
+
+	for (int i = 0; i < 20; i++){
+	    a[i] = r.nextInt(100);
+	}
+
+	System.out.println(a.toString());
+	bubbleSort(a.toString());
+	System.out.println(a.toString());
+    }
+
+}
